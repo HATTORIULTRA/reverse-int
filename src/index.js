69c76c5e8e875fc311dc85c33959b7614c343759
HATTORIULTRA <throwbvck@gmail.com>
2024-07-31 23:07:14 +0500

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const positiveNumber = Math.abs(n);
+    const numberToArray = [...positiveNumber.toString()];
+    const reversedArray = numberToArray.reverse();
+    const arrayToNumber = +reversedArray.join('');
+
+    return arrayToNumber;
 }
